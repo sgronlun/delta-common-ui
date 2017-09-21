bf9dbb7ee94faf1970f88f0dce69ccc94ad20fe6
Damien Morcellet <dmorcellet@free.fr>
2017-09-21 18:15:43 +0200

    Added a controller for the edition of selected items lists

---
+++ src/main/java/delta/common/ui/swing/lists/ListController.java
@@ -0,0 +1,178 @@
+package delta.common.ui.swing.lists;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+
+/**
+ * List controller.
+ * @param <T> Type of managed items.
+ */
+public class ListController<T>
+{
+  private JList<T> _list;
+  private DefaultListModel<T> _model;
+  private Comparator<T> _comparator;
+  private List<T> _items;
+
+  /**
+   * Constructor.
+   * @param comparator Comparator to sort displayed items, may be
+   * <code>null</code>.
+   */
+  public ListController(Comparator<T> comparator)
+  {
+    _comparator=comparator;
+    _items=new ArrayList<T>();
+    buildList();
+  }
+
+  private void buildList()
+  {
+    _list=new JList<T>();
+    Dimension d=new Dimension(300,300);
+    _list.setSize(d);
+    _list.setMinimumSize(d);
+
+    _model=new DefaultListModel<T>();
+    for(T item:_items)
+    {
+      _model.addElement(item);
+    }
+    _list.setModel(_model);
+  }
+
+  /**
+   * Get the managed JList.
+   * @return the managed JList.
+   */
+  public JList<T> getList()
+  {
+    return _list;
+  }
+
+  /**
+   * Get all the managed items.
+   * @return a possibly empty but not <code>null</code> list of items.
+   */
+  public List<T> getItems()
+  {
+    return _items;
+  }
+
+  /**
+   * Get the selected items.
+   * @return a possibly empty but not <code>null</code> list of items.
+   */
+  public List<T> getSelectedItems()
+  {
+    List<T> items=new ArrayList<T>();
+    if (_list!=null)
+    {
+      int[] selection=_list.getSelectedIndices();
+      for(int index:selection)
+      {
+        items.add(_items.get(index));
+      }
+    }
+    return items;
+  }
+
+  /**
+   * Set the items to use.
+   * @param list The items to use.
+   */
+  public void setItems(List<T> list)
+  {
+    _items.clear();
+    _items.addAll(list);
+    if (_comparator!=null)
+    {
+      Collections.sort(_items,_comparator);
+    }
+    if (_model!=null)
+    {
+      _model.clear();
+      for(T item:_items)
+      {
+        _model.addElement(item);
+      }
+    }
+  }
+
+  /**
+   * Add an item.
+   * The item is added at the end of the list, or sorted if a comparator is used.
+   * @param item Item to add.
+   */
+  public void addItem(T item)
+  {
+    _items.add(item);
+    if (_comparator!=null)
+    {
+      Collections.sort(_items,_comparator);
+      if (_model!=null)
+      {
+        int index=_items.indexOf(item);
+        _model.add(index,item);
+      }
+    }
+    else
+    {
+      if (_model!=null)
+      {
+        _model.addElement(item);
+      }
+    }
+  }
+
+  /**
+   * Remove an item.
+   * @param t Item to remove.
+   */
+  public void removeItem(T t)
+  {
+    _items.remove(t);
+    if (_model!=null)
+    {
+      _model.removeElement(t);
+    }
+  }
+
+  /**
+   * Remove some items.
+   * @param items Items to remove.
+   */
+  public void removeItems(List<T> items)
+  {
+    for(T item:items)
+    {
+      removeItem(item);
+    }
+  }
+
+  /**
+   * Remove all items.
+   */
+  public void removeAll()
+  {
+    _items.clear();
+    if (_model!=null)
+    {
+      _model.clear();
+    }
+  }
+
+  /**
+   * Release all managed resources.
+   */
+  public void dispose()
+  {
+    _list=null;
+  }
+}
---
+++ src/main/java/delta/common/ui/swing/lists/OrderedItemsSelectionController.java
@@ -0,0 +1,248 @@
+package delta.common.ui.swing.lists;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import delta.common.ui.swing.GuiFactory;
+
+/**
+ * Controller to edit an ordered selection of items.
+ * @param <T> Type of managed items.
+ */
+public class OrderedItemsSelectionController<T> implements ActionListener
+{
+  // Controllers
+  private ListController<T> _sourceList;
+  private ListController<T> _selectedList;
+  // UI
+  private JPanel _panel;
+
+  private JButton _add;
+  private JButton _addAll;
+  private JButton _remove;
+  private JButton _removeAll;
+
+  /**
+     * Constructor.
+     * @param comparator Comparator to use (may be <code>null</code>).
+     */
+    public OrderedItemsSelectionController(Comparator<T> comparator) {
+        _sourceList = new ListController<T>(comparator);
+        _selectedList = new ListController<T>(null);
+    }
+
+    /**
+     * Set the items to use.
+     * @param items The items to use.
+     */
+  public void setItems(List<T> items)
+  {
+    _sourceList.setItems(items);
+    _selectedList.removeAll();
+    updateButtonsStatus();
+  }
+
+  /**
+   * Select all items.
+   */
+  public void selectAll()
+  {
+    List<T> items=_sourceList.getItems();
+    _selectedList.setItems(items);
+    _sourceList.removeAll();
+    updateButtonsStatus();
+  }
+
+  /**
+   * Select some items.
+   * @param items Items to select.
+   */
+  public void selectItems(List<T> items)
+  {
+    _selectedList.setItems(items);
+    _sourceList.removeItems(items);
+  }
+
+  /**
+   * Get the selected items.
+   * @return a possibly empty but not <code>null</code> list of items.
+   */
+  public List<T> getSelectedItems()
+  {
+    return _selectedList.getItems();
+  }
+
+  /**
+   * Get the managed panel.
+   * @return the managed panel.
+   */
+  public JPanel getPanel()
+  {
+    if (_panel==null)
+    {
+      _panel=build();
+    }
+    return _panel;
+  }
+
+  private JPanel build()
+  {
+    JPanel panel=GuiFactory.buildPanel(new GridBagLayout());
+
+    JList<T> sourceList=_sourceList.getList();
+    JList<T> selectedList=_selectedList.getList();
+    ListSelectionListener selectionListener=new ListSelectionListener()
+    {
+      public void valueChanged(ListSelectionEvent e)
+      {
+        if (e.getValueIsAdjusting()) return;
+        updateButtonsStatus();
+      }
+    };
+    sourceList.addListSelectionListener(selectionListener);
+    JScrollPane sourceSP=new JScrollPane(sourceList);
+    Dimension d=new Dimension(300,200);
+    sourceSP.setPreferredSize(d);
+    sourceSP.setBorder(new TitledBorder(BorderFactory.createEtchedBorder(Color.white,new Color(134,134,134)),"Selectable"));
+    selectedList.addListSelectionListener(selectionListener);
+    JScrollPane selectedSP=new JScrollPane(selectedList);
+    selectedSP.setPreferredSize(d);
+    selectedSP.setBorder(new TitledBorder(BorderFactory.createEtchedBorder(Color.white,new Color(134,134,134)),"Selected"));
+
+    JPanel buttonsPanel=buildButtonsPanel();
+    GridBagConstraints c=new GridBagConstraints(0,0,1,1,1.0,1.0,GridBagConstraints.CENTER,GridBagConstraints.BOTH,new Insets(5,5,5,5),0,0);
+    panel.add(sourceSP,c);
+    c=new GridBagConstraints(1,0,1,1,0.0,0.0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+    panel.add(buttonsPanel,c);
+    c=new GridBagConstraints(2,0,1,1,1.0,1.0,GridBagConstraints.CENTER,GridBagConstraints.BOTH,new Insets(5,5,5,5),0,0);
+    panel.add(selectedSP,c);
+    updateButtonsStatus();
+    return panel;
+  }
+
+  private JPanel buildButtonsPanel()
+  {
+    JPanel buttonsPanel=new JPanel(new GridBagLayout());
+    GridBagConstraints c=new GridBagConstraints(0,0,1,1,1.0,1.0,GridBagConstraints.CENTER,GridBagConstraints.HORIZONTAL,new Insets(5,5,5,5),0,0);
+    _add=new JButton(">");
+    buttonsPanel.add(_add,c);
+    c.gridy++;
+    _add.addActionListener(this);
+    _addAll=new JButton(">>");
+    buttonsPanel.add(_addAll,c);
+    c.gridy++;
+    _addAll.addActionListener(this);
+    _remove=new JButton("<");
+    buttonsPanel.add(_remove,c);
+    c.gridy++;
+    _remove.addActionListener(this);
+    _removeAll=new JButton("<<");
+    buttonsPanel.add(_removeAll,c);
+    c.gridy++;
+    _removeAll.addActionListener(this);
+    return buttonsPanel;
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    Object component=e.getSource();
+    if (component==_add)
+    {
+      List<T> selectedItems=_sourceList.getSelectedItems();
+      for(T selectedItem:selectedItems)
+      {
+        _selectedList.addItem(selectedItem);
+        _sourceList.removeItem(selectedItem);
+      }
+    }
+    else if (component==_addAll)
+    {
+      List<T> selectedItems=_sourceList.getItems();
+      for(T selectedItem:selectedItems)
+      {
+        _selectedList.addItem(selectedItem);
+      }
+      _sourceList.removeAll();
+    }
+    else if (component==_remove)
+    {
+      List<T> selectedItems=_selectedList.getSelectedItems();
+      for(T selectedItem:selectedItems)
+      {
+        _sourceList.addItem(selectedItem);
+        _selectedList.removeItem(selectedItem);
+      }
+    }
+    else if (component==_removeAll)
+    {
+      List<T> selectedItems=_selectedList.getItems();
+      for(T selectedItem:selectedItems)
+      {
+        _sourceList.addItem(selectedItem);
+      }
+      _selectedList.removeAll();
+    }
+    updateButtonsStatus();
+  }
+
+  private void updateButtonsStatus()
+  {
+    if (_add!=null)
+    {
+      List<T> sourceSelection=_sourceList.getSelectedItems();
+      _add.setEnabled(sourceSelection.size()>0);
+    }
+    if (_addAll!=null)
+    {
+      List<T> items=_sourceList.getItems();
+      _addAll.setEnabled(items.size()>0);
+    }
+    if (_remove!=null)
+    {
+      List<T> selectedSelection=_selectedList.getSelectedItems();
+      _remove.setEnabled(selectedSelection.size()>0);
+    }
+    if (_removeAll!=null)
+    {
+      List<T> selected=_selectedList.getItems();
+      _removeAll.setEnabled(selected.size()>0);
+    }
+  }
+
+  /**
+   * Release all managed resources.
+   */
+  public void dispose()
+  {
+    if (_sourceList!=null)
+    {
+      _sourceList.dispose();
+      _sourceList=null;
+    }
+    if (_selectedList!=null)
+    {
+      _selectedList.dispose();
+      _selectedList=null;
+    }
+    _add=null;
+    _addAll=null;
+    _remove=null;
+    _removeAll=null;
+  }
+}
