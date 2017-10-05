5569963289e35997da30919bbf60180b38c3ea64
Damien Morcellet <dmorcellet@free.fr>
2017-10-05 12:38:15 +0200

    Added missing Javadoc for the LabelProvider

--- src/main/java/delta/common/ui/swing/lists/LabelProvider.java
+++ src/main/java/delta/common/ui/swing/lists/LabelProvider.java
@@ -1,9 +1,16 @@
 package delta.common.ui.swing.lists;
 
 /**
- * @author dm
+ * Interface of a label provider.
+ * @author DAM
+ * @param <T> Type of managed items.
  */
 public interface LabelProvider<T>
 {
+  /**
+   * Get a label for the given item.
+   * @param item Item to use.
+   * @return A label.
+   */
   String getLabel(T item);
 }
